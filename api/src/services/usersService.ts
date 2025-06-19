3f0a8de5e2d6029f727a70d1e1d1ba43aa58f5c1
Alex <chuchanouski44@gmail.com>
2025-06-19 12:56:21 +0300

    fix user service

--- api/src/services/usersService.ts
+++ api/src/services/usersService.ts
@@ -1,12 +1,13 @@
+import { Not } from "typeorm";
 import { AppDataSource } from "../config/database";
 import { User } from "../models/User";
 
 export class UsersService {
-  async getUsers(userId: number) {
+  async getUsers(currentUserId: number) {
     const usersRepository = AppDataSource.getRepository(User);
 
     const user = await usersRepository.find({
-      // where: { id: userId },
+      where: { id: Not(currentUserId) },
       select: ["id", "username", "email"],
     });
 
